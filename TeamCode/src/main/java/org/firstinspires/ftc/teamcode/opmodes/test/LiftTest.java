f66767a91a5211103c4d0e97b0faed6f5e508cc4
cdbbnny <cdbbnnycode@users.noreply.github.com>
2020-12-15 15:05:29 -0600

    Consolidate test opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/LiftTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/LiftTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmodes.util;
+package org.firstinspires.ftc.teamcode.opmodes.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
