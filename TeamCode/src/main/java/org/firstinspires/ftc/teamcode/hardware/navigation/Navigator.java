fa17ad9401a2e5eb565b42b9487e02c91a7905e6
Siddappa <44073565+Siddapa@users.noreply.github.com>
2021-02-02 15:40:53 -0600

    Completed goTo Navigator function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/navigation/Navigator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/navigation/Navigator.java
@@ -1,16 +1,22 @@
 package org.firstinspires.ftc.teamcode.hardware.navigation;
 
-import com.qualcomm.ftccommon.configuration.EditServoControllerActivity;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.hardware.Drivetrain;
 import org.firstinspires.ftc.teamcode.hardware.IMU;
+import org.firstinspires.ftc.teamcode.hardware.events.NavMoveEvent;
+import org.firstinspires.ftc.teamcode.util.event.EventBus;
+import org.firstinspires.ftc.teamcode.util.event.EventBus.Subscriber;
+import org.firstinspires.ftc.teamcode.util.event.EventFlow;
 
 public class Navigator
 {
     private Drivetrain drivetrain;
     private Odometry odometry;
     private IMU imu;
+    private EventBus ev;
+
+    private EventFlow goTo;
     
     private double fwdTarget;
     private double angleTarget;
@@ -24,6 +30,7 @@ public class Navigator
         this.drivetrain = drivetrain;
         this.odometry = odo;
         imu = odo.getIMU();
+        goTo = new EventFlow(ev);
     }
     
     public void setForwardSpeed(double fwdSpeed)
@@ -50,6 +57,16 @@ public class Navigator
         double heading = imu.getHeading();
         double turnError = angleTarget - heading;
         double turnPower = Range.clip(turnError * turnKp, -turnSpeed, turnSpeed);
+
+
+        if (ev != null){
+            if (turnError < 10 && turnError > 10){
+                ev.pushEvent(new NavMoveEvent(NavMoveEvent.TURN_COMPLETE));
+            }
+            if (fwdError < 10 && fwdError > 10){
+                ev.pushEvent(new NavMoveEvent(NavMoveEvent.FORWARD_COMPLETE));
+            }
+        }
         
         // Actually drive
         drivetrain.telemove(fwdPower, turnPower);
@@ -77,6 +94,21 @@ public class Navigator
     
     public void goTo(double x, double y, double finHeading)
     {
-        // TODO
+        double targetHeading = Math.toDegrees(Math.atan2(y, x));
+        if (targetHeading < 0) targetHeading = 360 - Math.abs(targetHeading);
+        final double finalTargetHeading = targetHeading;
+        goTo.start(new Subscriber<>(NavMoveEvent.class, (ev, bus, sub) -> {
+            turnAbs(finalTargetHeading);
+        }, "Direction Aim", NavMoveEvent.TURN_COMPLETE))
+        .then(new Subscriber<>(NavMoveEvent.class, (ev, bus, sub) -> {
+            forward(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
+        }, "Move Forward", NavMoveEvent.FORWARD_COMPLETE))
+        .then(new Subscriber<>(NavMoveEvent.class, (ev, bus, sub) -> {
+            turnAbs(finHeading);
+        }, "Final Turn", NavMoveEvent.TURN_COMPLETE));
+    }
+
+    private void connectEventBus(EventBus ev){
+        this.ev = ev;
     }
 }
