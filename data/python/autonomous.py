da7ff4cdfaece5faa17b124214425a2921dd9686
TarunA123 <TarunA123@users.noreply.github.com>
2021-04-23 13:45:04 -0500

    Auto changes

--- data/python/autonomous.py
+++ data/python/autonomous.py
@@ -7,7 +7,7 @@ LIFECYCLE_EVENT_INIT = 0
 LIFECYCLE_EVENT_START = 1
 LIFECYCLE_EVENT_STOP = 2
 
-SHOOT_SPEED = 0.6725
+SHOOT_SPEED = 0.7225
 
 def place_wobble(nav, dropit=False):
     if not dropit:
@@ -72,14 +72,14 @@ def run_auto(nav):
         nav.turn(25)
 
     nav.actuator('shooter', {'action': 'start', 'speed': SHOOT_SPEED})
-    place_wobble(nav, dropit=(rings_seen == 4))
+    place_wobble(nav)
 
     if rings_seen == 4:
         nav.move(-60, 0, reverse=False, speed=0.9)
 
     # Line up for shooting
     nav.move(-58.5, 0, reverse=False)
-    nav.actuator('turret', {'action': 'rotate', 'angle': 0.1625})
+    nav.actuator('turret', {'action': 'rotate', 'angle': 0.16})
     nav.turn(90)
     if rings_seen == 0:
         time.sleep(3)
@@ -104,11 +104,11 @@ def run_auto(nav):
     elif rings_seen == 4:
         nav.actuator('turret', {'action': 'home'})
         # nav.turn(-3)
-        nav.actuator('intake', {'action': 'intake', 'speed': -0.6})
-        nav.move(-46, 5, reverse=False, speed=0.9)
-        nav.actuator('intake', {'action': 'intake'})
+        nav.actuator('intake', {'action': 'intake', 'speed': -0.75})
+        nav.move(-46, 5, reverse=False, speed=0.85)
+        nav.actuator('intake', {'action': 'intake', 'speed': 0.82})
         time.sleep(0.15)
-        nav.move(-37, 10, reverse=False, speed=0.4)
+        nav.move(-37, 12, reverse=False, speed=0.45)
         time.sleep(1)
         nav.move(-58, 2)
         nav.turn(0)
@@ -126,11 +126,11 @@ def run_auto(nav):
     #else:
         # second wobble
     if rings_seen == 0 or rings_seen == 1:
-        nav.move(-27.5, 16)
+        nav.move(-27.5, 17)
         nav.turn(192)
         time.sleep(0.75)
     elif rings_seen == 4:
-        nav.move(-27.5, 16)
+        nav.move(-27, 18)
         time.sleep(0.25)
 
     pick_wobble(nav)
@@ -145,7 +145,7 @@ def run_auto(nav):
         # nav.move(-47, 18)
         nav.move(-103, -13, speed=0.9)
         # nav.turn(-323)
-    place_wobble(nav, dropit=(rings_seen == 4))
+    place_wobble(nav)
 
     nav.move(-76, 0, reverse=(rings_seen != 4))
 
