f0141c68c36963407c3a88228d9102ee573903ef
Abhinav-Narayan-Anthati <90067282+Abhinav-Narayan-Anthati@users.noreply.github.com>
2021-11-09 19:56:22 -0600

    Added Claw Functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/TestMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/TestMotors.java
@@ -4,16 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp()
 public class TestMotors extends OpMode
 {
-
+    Servo one;
     @Override
     public void init() {
-
+        one = hardwareMap.get(Servo.class, "one");
     }
 
     @Override
@@ -27,6 +28,11 @@ public class TestMotors extends OpMode
     }
     @Override
     public void loop() {
+        if (gamepad1.x){
+            one.setPosition(20);
+        } else if (gamepad1.a){
+            one.setPosition(0);
+        }
 
     }
 
