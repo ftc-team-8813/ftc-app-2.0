cf982a48586d42a315eb9712b2a0319b7aaf1d6b
Reetham Gubba <gubba.ricky@gmail.com>
2022-03-20 13:39:40 -0500

    Lift Control, Arm Movement, Arm Pivot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/ReethamLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/ReethamLift.java
@@ -1,2 +1,64 @@
-package org.firstinspires.ftc.teamcode.hardware;public class ReethamLift {
+package org.firstinspires.ftc.teamcode.hardware;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class ReethamLift {
+
+    DcMotor motor1;
+    DcMotor motor2;
+    DcMotor pivoter;
+    double finalDoubleValue;
+    double kp;
+    double target;
+
+    public ReethamLift(DcMotor motor1, DcMotor motor2, DcMotor pivoter, double kp){
+        motor1 = this.motor1;
+        motor2 = this.motor2;
+        kp = this.kp;
+        pivoter = this.pivoter;
+        finalDoubleValue = 0;
+        target = 0;
+    }
+
+    public void set_DcMotor_power(DcMotor motor1, DcMotor motor2, double power){
+        motor1.setPower(power);
+        motor2.setPower(power);
+    }
+
+    public void lift_motors_extension(int ticks){
+        motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor1.setTargetPosition(ticks); //Can depend because I don't know the exact target tick position
+        motor2.setTargetPosition(ticks);
+        set_DcMotor_power(motor1, motor2, 1);
+        motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while(motor1.isBusy() && motor2.isBusy()){
+            telemetry.addData("2 Motors Status", "Motors are busy");
+            telemetry.update();
+        }
+        set_DcMotor_power(motor1, motor2, 0);
+    }
+
+    public void set_pivot(DcMotor pivotMotor, double increment, boolean doneWithPivot){
+        double error = (target + increment) - pivotMotor.getCurrentPosition();
+        finalDoubleValue = error * kp;
+        pivotMotor.setPower(error * kp);
+        pivotMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        if(pivotMotor.getCurrentPosition() == finalDoubleValue){
+            doneWithPivot = true;
+        }
+    }
+
+    public void pivot_motor_extension(int ticks){
+        pivoter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        pivoter.setTargetPosition(ticks);
+        pivoter.setPower(1);
+        pivoter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while(pivoter.isBusy()){
+            telemetry.addData("Pivot Motor Status", "Motor is busy");
+            telemetry.update();
+        }
+        pivoter.setPower(0);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ReethamLiftControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ReethamLiftControl.java
@@ -1,2 +1,53 @@
-package org.firstinspires.ftc.teamcode.opmodes.teleop;public class ReethamLiftControl {
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.hardware.ReethamLift;
+
+public class ReethamLiftControl extends OpMode {
+        ReethamLift lift;
+        DcMotor Motor1;
+        DcMotor Motor2;
+        DcMotor pivoter;
+        boolean lift_motors_out;
+        boolean pivot_motor_out;
+        boolean donePivoting;
+
+        @Override
+        public void init() {
+                lift = new ReethamLift(Motor1, Motor2, pivoter, 0.001);
+        }
+
+        @Override
+        public void loop() {
+                if(gamepad1.dpad_up) {
+                        lift.lift_motors_extension(1000);
+                        if(Motor1.getCurrentPosition() == 1000 && Motor2.getCurrentPosition() == 1000){
+                                lift_motors_out = true;
+                        }
+                }else if(lift_motors_out){
+                        lift.set_pivot(pivoter, gamepad1.left_stick_x, donePivoting);
+                }else if(donePivoting){
+                        lift.pivot_motor_extension(1000);
+                        if(pivoter.getCurrentPosition() == 1000){
+                                pivot_motor_out = true;
+                        }
+                }else if(gamepad1.dpad_down){
+                        if(pivot_motor_out){
+                                lift.pivot_motor_extension(0);
+                                        if(pivoter.getCurrentPosition() == 0){
+                                                pivot_motor_out = false;
+                                        }
+
+                                lift.lift_motors_extension(0);
+                                        if(Motor1.getCurrentPosition() == 0 && Motor2.getCurrentPosition() == 0){
+                                                lift_motors_out = false;
+                                        }
+                        }else if(lift_motors_out){
+                                lift.lift_motors_extension(0);
+                                if(Motor1.getCurrentPosition() == 0 && Motor2.getCurrentPosition() == 0){
+                                        lift_motors_out = false;
+                                }
+                        }
+                }
+        }
 }
