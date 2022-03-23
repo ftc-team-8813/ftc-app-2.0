e25491d34c0aa8891a93075c2539d252be22c120
Abhinav-Anthati <abhinav.anthati@gmail.com>
2022-03-23 18:35:36 -0500

    Outtaking and Intaking Automation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CurrentTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CurrentTele.java
@@ -8,6 +8,7 @@ import org.firstinspires.ftc.teamcode.hardware.Robot;
 import org.firstinspires.ftc.teamcode.input.ControllerMap;
 import org.firstinspires.ftc.teamcode.opmodes.teleop.ControlMgr;
 import org.firstinspires.ftc.teamcode.opmodes.teleop.DriveControl;
+import org.firstinspires.ftc.teamcode.opmodes.teleop.IntakeControl;
 import org.firstinspires.ftc.teamcode.opmodes.teleop.LiftControl;
 import org.firstinspires.ftc.teamcode.opmodes.teleop.ServerControl;
 import org.firstinspires.ftc.teamcode.util.Logger;
@@ -50,6 +51,7 @@ public class CurrentTele extends LoggingOpMode
 //        controlMgr.addModule(new ServerControl("Server Control"));
         controlMgr.addModule(new DriveControl("Drive Control"));
         controlMgr.addModule(new LiftControl("Lift Control"));
+        controlMgr.addModule(new IntakeControl("Intake Control"));
 
         web_host.index();
         controlMgr.initModules();
