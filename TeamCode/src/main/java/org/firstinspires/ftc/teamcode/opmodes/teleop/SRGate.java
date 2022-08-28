a04620c4259ca621a5266f5b26e99d1f1e6792f4
Sai Reetham Gubba <gubba.ricky@gmail.com>
2022-08-28 10:13:52 -0500

    Summer Robot Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/SRGate.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class SRGate {
+    Servo gate;
+
+    public SRGate(Servo gate){
+        this.gate = gate;
+    }
+}
