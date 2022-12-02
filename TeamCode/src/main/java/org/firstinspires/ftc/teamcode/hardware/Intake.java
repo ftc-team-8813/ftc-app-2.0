1f65c29ce2b9b72a07ecfdf24c715229d80ea9b4
Sai Reetham Gubba <gubba.ricky@gmail.com>
2022-12-02 17:41:41 -0600

    Intake and Lift Hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.java
@@ -3,34 +3,95 @@ package org.firstinspires.ftc.teamcode.hardware;
 import com.qualcomm.hardware.broadcom.BroadcomColorSensor;
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Intake {
 
-    private final Servo claw;
-    private final DistanceSensor claw_sensor;
-    private boolean intaken = false;
+    DcMotorEx horiz;
+    DcMotorEx arm;
+    DigitalChannel horiz_limit;
+    DigitalChannel arm_limit;
+    DistanceSensor claw_sens;
+    Servo claw;
+    double[] encoderVals = new double[2];
+    boolean[] limitValues = new boolean[2];
 
-    public Intake(Servo claw, DistanceSensor claw_sensor){
+    public Intake(DcMotorEx horiz, DcMotorEx arm, DigitalChannel horiz_limit, DigitalChannel arm_limit, DistanceSensor claw_sens, Servo claw){
+        this.horiz = horiz;
+        this.arm = arm;
+        this.horiz_limit = horiz_limit;
+        this.arm_limit = arm_limit;
+        this.claw_sens = claw_sens;
         this.claw = claw;
-        this.claw_sensor = claw_sensor;
-    }
-    public void setClaw(double pos) {
-        claw.setPosition(pos);
     }
 
-    public double getDistance() {
-        return claw_sensor.getDistance(DistanceUnit.MM);
+    public void resetIntakeEncoders(){
+        horiz.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        horiz.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
-    public double getClawPosition() {
+    public void setArmTarget(int position){
+        arm.setTargetPosition(position);
+    }
+
+    public double getArmTarget(){
+        return arm.getTargetPosition();
+    }
+
+    public void setClaw(double position){
+        claw.setPosition(position);
+    }
+
+    public double getClaw(double pos){
         return claw.getPosition();
     }
 
+    public void setArmPow(double pow){
+        arm.setPower(pow);
+    }
+
+    public void setHorizPow(double pow){
+        horiz.setPower(pow);
+    }
+
+    public void setHorizTarget (int position){
+        horiz.setTargetPosition(position);
+    }
+
+    public double getHorizTarget(){
+        return horiz.getTargetPosition();
+    }
+
+    public void setHoriz_limit(boolean state){
+        horiz_limit.setState(state);
+    }
+
+    public void setArm_limit(boolean state){
+        arm_limit.setState(state);
+    }
+
+    public boolean[] limitValues(){
+        limitValues[0] = arm_limit.getState();
+        limitValues[1] = horiz_limit.getState();
+
+        return limitValues;
+    }
+
+    public double[] getEncoderVals(){
+        encoderVals[0] = arm.getCurrentPosition();
+        encoderVals[1] = horiz.getCurrentPosition();
+        return encoderVals;
+    }
+
+    public double getDistance() {
+        return claw_sens.getDistance(DistanceUnit.MM);
+    }
 }
