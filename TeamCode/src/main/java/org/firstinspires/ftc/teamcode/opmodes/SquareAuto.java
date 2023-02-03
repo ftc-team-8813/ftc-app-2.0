ee3a74bb634e13565229bb77690dd2c2c8ffa9ee
Abhinav-Anthati <abhinav.anthati@gmail.com>
2023-02-03 09:43:28 -0600

    Semi-Fuunctional Six Cone Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SquareAuto.java
@@ -0,0 +1,247 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.hardware.Arm;
+import org.firstinspires.ftc.teamcode.hardware.Drivetrain;
+import org.firstinspires.ftc.teamcode.hardware.Horizontal;
+import org.firstinspires.ftc.teamcode.hardware.Intake;
+import org.firstinspires.ftc.teamcode.hardware.Lift;
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.hardware.navigation.Odometry;
+import org.firstinspires.ftc.teamcode.hardware.navigation.PID;
+import org.firstinspires.ftc.teamcode.util.Logger;
+import org.firstinspires.ftc.teamcode.util.LoopTimer;
+import org.firstinspires.ftc.teamcode.vision.AprilTagDetectionPipeline;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
+@Autonomous(name = "Z Auto")
+public class ZAuto extends LoggingOpMode{
+
+    private Lift lift;
+    private Horizontal horizontal;
+    private Arm arm;
+    private Intake intake;
+    private Drivetrain drivetrain;
+    private Odometry odometry;
+
+    private String result = "Nothing";
+
+    private int main_id = 0;
+    private int arm_id = 0;
+
+    private OpenCvCamera camera;
+    private AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    private static final double FEET_PER_METER = 3.28084;
+
+    private double fx = 578.272;
+    private double fy = 578.272;
+    private double cx = 402.145;
+    private double cy = 221.506;
+
+    private double tagsize = 0.166;
+
+    private final PID arm_PID = new PID(0.009, 0, 0, 0.1, 0, 0);
+    private final PID horizontal_PID = new PID(0.01, 0, 0, 0, 0, 0);
+    private final PID lift_PID = new PID(0.02, 0, 0, 0.015, 0, 0);
+
+    private ElapsedTime timer = new ElapsedTime();
+
+    private ElapsedTime lift_trapezoid = new ElapsedTime();;
+    private double lift_accel = 0.27;
+
+    private double lift_target = 0;
+    private double horizontal_target = 0;
+    private double arm_target = 0;
+
+    private ElapsedTime liftTimer = new ElapsedTime();
+    private boolean liftTimerReset = false;
+
+    private final Logger log = new Logger("Square Auto");
+
+    @Override
+    public void init() {
+        super.init();
+        Robot robot = Robot.initialize(hardwareMap);
+        lift = robot.lift;
+        horizontal = robot.horizontal;
+        arm = robot.arm;
+        intake = robot.intake;
+        drivetrain = robot.drivetrain;
+        odometry = robot.odometry;
+
+        odometry.Down();
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+//        telemetry.setMsTransmissionInterval(50);
+
+        odometry.resetEncoders();
+    }
+
+    @Override
+    public void init_loop() {
+        super.init_loop();
+
+        ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+        if(currentDetections.size() != 0) {
+
+            for (AprilTagDetection tag : currentDetections) {
+                if (tag.id == 107) {
+                    result = "FTC8813: 1";
+                    break;
+                }
+                else if (tag.id == 350) {
+                    result = "FTC8813: 2";
+                    break;
+                }
+                else if (tag.id == 25) {
+                    result = "FTC8813: 3";
+                    break;
+                }
+                else {
+                    result = "Nothing";
+                }
+
+            }
+        }
+
+
+        telemetry.addData("Detected", result);
+
+        telemetry.update();
+
+        arm.resetEncoders();
+        lift.resetEncoders();
+        horizontal.resetEncoders();
+        odometry.resetEncoders();
+    }
+
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void loop() {
+
+        odometry.updatePose(-drivetrain.getHeading());
+
+        switch (main_id) {
+            case 0:
+                drivetrain.autoMove(0,-72,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+            case 1:
+                drivetrain.autoMove(-24,-72,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+            case 2:
+                drivetrain.autoMove(-24,0,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+            case 3:
+                drivetrain.autoMove(-48,0,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+            case 4:
+                drivetrain.autoMove(-48,-72,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+            case 5:
+                drivetrain.autoMove(-72,-72,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+            case 6:
+                drivetrain.autoMove(-72,0,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+            case 7:
+                drivetrain.autoMove(-48,0,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+            case 8:
+                drivetrain.autoMove(-48,-72,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+            case 9:
+                drivetrain.autoMove(-24,-72,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+            case 10:
+                drivetrain.autoMove(-24,0,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+            case 11:
+                drivetrain.autoMove(0,0,0,1,1,2, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                }
+                break;
+        }
+
+        drivetrain.update(odometry.getPose(), telemetry,false);
+
+        telemetry.addData("Main ID", main_id);
+        telemetry.addData("Loop Time: ", LoopTimer.getLoopTime());
+        telemetry.update();
+
+        LoopTimer.resetTimer();
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/TurnAuto.java
@@ -0,0 +1,215 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.hardware.Arm;
+import org.firstinspires.ftc.teamcode.hardware.Drivetrain;
+import org.firstinspires.ftc.teamcode.hardware.Horizontal;
+import org.firstinspires.ftc.teamcode.hardware.Intake;
+import org.firstinspires.ftc.teamcode.hardware.Lift;
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.hardware.navigation.Odometry;
+import org.firstinspires.ftc.teamcode.hardware.navigation.PID;
+import org.firstinspires.ftc.teamcode.util.Logger;
+import org.firstinspires.ftc.teamcode.util.LoopTimer;
+import org.firstinspires.ftc.teamcode.vision.AprilTagDetectionPipeline;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
+@Autonomous(name = "Turn Auto")
+public class TurnAuto extends LoggingOpMode{
+
+    private Lift lift;
+    private Horizontal horizontal;
+    private Arm arm;
+    private Intake intake;
+    private Drivetrain drivetrain;
+    private Odometry odometry;
+
+    private String result = "Nothing";
+
+    private int main_id = 0;
+    private int arm_id = 0;
+
+    private OpenCvCamera camera;
+    private AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    private static final double FEET_PER_METER = 3.28084;
+
+    private double fx = 578.272;
+    private double fy = 578.272;
+    private double cx = 402.145;
+    private double cy = 221.506;
+
+    private double tagsize = 0.166;
+
+    private final PID arm_PID = new PID(0.009, 0, 0, 0.1, 0, 0);
+    private final PID horizontal_PID = new PID(0.01, 0, 0, 0, 0, 0);
+    private final PID lift_PID = new PID(0.02, 0, 0, 0.015, 0, 0);
+
+    private ElapsedTime timer = new ElapsedTime();
+
+    private ElapsedTime lift_trapezoid = new ElapsedTime();;
+    private double lift_accel = 0.27;
+
+    private double lift_target = 0;
+    private double horizontal_target = 0;
+    private double arm_target = 0;
+
+    private ElapsedTime liftTimer = new ElapsedTime();
+    private boolean liftTimerReset = false;
+
+    private final Logger log = new Logger("Square Auto");
+
+    @Override
+    public void init() {
+        super.init();
+        Robot robot = Robot.initialize(hardwareMap);
+        lift = robot.lift;
+        horizontal = robot.horizontal;
+        arm = robot.arm;
+        intake = robot.intake;
+        drivetrain = robot.drivetrain;
+        odometry = robot.odometry;
+
+        odometry.Down();
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+//        telemetry.setMsTransmissionInterval(50);
+
+        odometry.resetEncoders();
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+    }
+
+    @Override
+    public void init_loop() {
+        super.init_loop();
+
+        ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+        if(currentDetections.size() != 0) {
+
+            for (AprilTagDetection tag : currentDetections) {
+                if (tag.id == 107) {
+                    result = "FTC8813: 1";
+                    break;
+                }
+                else if (tag.id == 350) {
+                    result = "FTC8813: 2";
+                    break;
+                }
+                else if (tag.id == 25) {
+                    result = "FTC8813: 3";
+                    break;
+                }
+                else {
+                    result = "Nothing";
+                }
+
+            }
+        }
+
+
+        telemetry.addData("Detected", result);
+
+        telemetry.update();
+
+        arm.resetEncoders();
+        lift.resetEncoders();
+        horizontal.resetEncoders();
+        odometry.resetEncoders();
+    }
+
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void loop() {
+
+        odometry.updatePose(-drivetrain.getHeading());
+
+        switch (main_id) {
+            case 0:
+                drivetrain.autoMove(0,0,0,1,1,1, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                    timer.reset();
+                }
+                break;
+            case 1:
+                if (timer.seconds() > 3) {
+                    main_id += 1;
+                }
+                break;
+            case 2:
+                drivetrain.autoMove(0,0,90,1,1,1, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                    timer.reset();
+                }
+                break;
+            case 3:
+                if (timer.seconds() > 3) {
+                    main_id += 1;
+                }
+                break;
+            case 4:
+                drivetrain.autoMove(0,0,270,1,1,1, odometry.getPose(), telemetry);
+                if (drivetrain.hasReached()) {
+                    main_id += 1;
+                    timer.reset();
+                }
+                break;
+            case 5:
+                if (timer.seconds() > 3) {
+                    main_id = 0;
+                }
+                break;
+
+        }
+
+        drivetrain.update(odometry.getPose(), telemetry,false);
+
+        telemetry.addData("Main ID", main_id);
+        telemetry.addData("Loop Time: ", LoopTimer.getLoopTime());
+        telemetry.update();
+
+        LoopTimer.resetTimer();
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+
+}
