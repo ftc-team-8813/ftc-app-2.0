fcec71667ef27b1969295706f237b886d0595ac9
Ice Maker <43729283+ice-maker@users.noreply.github.com>
2023-02-03 17:54:18 -0600

    untested motion profile class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/navigation/MotionProfile.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.hardware.navigation;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+public class MotionProfile {
+    private double min = 0.2;
+    private double max = 1.0;
+
+    private double output = 1.0;
+
+    private double error;
+
+    private ElapsedTime rise_timer;
+
+    private double rise_slope = 1;
+    private double fall_slope = 1;
+
+    private String old_id;
+
+    public MotionProfile(double rise_slope, double fall_slope, double min, double max) {
+        rise_timer = new ElapsedTime();
+        this.rise_slope = rise_slope;
+        this.fall_slope = fall_slope;
+        this.min = min;
+        this.max = max;
+    }
+
+    public void setBounds(double min, double max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public void setSlopes(double rise_slope, double fall_slope) {
+        this.rise_slope = rise_slope;
+        this.fall_slope = fall_slope;
+    }
+
+    public void updateMotionProfile(String id, boolean rise, boolean fall) {
+        if (old_id != id) {
+            rise_timer.reset();
+            old_id = id;
+        }
+        if (rise) {
+            output = Math.min(rise_timer.seconds() * rise_slope, output);
+        }
+        if (fall) {
+            output = Math.min(error * fall_slope, output);
+        }
+
+        if (!rise && !fall) {
+            output = 1.0;
+        }
+
+        output = Range.clip(output, min, max);
+    }
+
+    public double getProfiledPower(double error, double power) {
+        this.error = Math.abs(error);
+        return Range.clip(power, -output, output);
+    }
+}
